c358273337cda997d69f5290f26560e2b2ac9a9e
AngelD <angelduarteing@gmail.com>
2023-02-22 12:51:10 -0500

    ibm curse

---
+++ Archivos/open_read.py
@@ -0,0 +1,8 @@
+with open("Archivos//ejemplo1.txt","r") as ejemplo1:
+    print(ejemplo1.readline())
+    print(ejemplo1.readline())
+    print(ejemplo1.readline())
+    
+
+with open("prueba//prueba.txt","r") as ejemplo2:
+    print(ejemplo2.readline())
---
+++ Archivos/open_write.py
@@ -0,0 +1,6 @@
+with open("prueba//prueba.txt","w") as ejemplo1:
+    ejemplo1.write("la realidad es parte de una creencia o creación \n")
+    ejemplo1.write("es infinita o tan solo no existe \nla percepcion de lo tangible puede cambiar")
+    
+with open("prueba//prueba.txt","r") as ejemplo1:   
+    print(ejemplo1.read())
