411c46b6250c60d7b6b843aaa179b95dac12c6e5
AngelD <angelduarteing@gmail.com>
2023-02-24 18:52:10 -0500

    mas

--- Numpy/API.py
+++ Numpy/API.py
@@ -1,2 +1,19 @@
 # API: Application Program Interfaces
 # Una API permite que dos piezas de software se comuniquen entre sí.
+
+from randomuser import RandomUser
+import pandas as pd
+
+# RandomUsers(): crea una lista de usuarios random
+r = RandomUser()
+some_list = r.generate_users(10)
+
+# get_full_name(): crean nombnres para cada usuario
+for user in some_list:
+    print (user.get_full_name()," ",user.get_email())
+
+# get_full_name(): crean fotos para cada usuario
+for user in some_list:
+    print (user.get_picture())
+    
+  
---
+++ Numpy/prueba.py
@@ -0,0 +1,9 @@
+#print('1,2,3,4'.split(','))
+A=[1,'a'] 
+B=[2,1,'d']
+
+V={'A','B'}
+V.add('C')
+print(V)
+V.add('C')
+print(V)
